5dbb07a7ff2e8abef03e55bf6940da11965b5fd4
Shubham Kumar <hello@shubhkumar.in>
2020-11-22 09:12:32 +0530

    Added to only Process htb,hacktheboxtags.

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ async function setupHTB(db) {
         async updateDB({
             title,
             permalink,
-            tags
+            tags,
+            machineId
         }, hexoPublished) {
             await db.read();
             const published = db.get('published').find({
@@ -79,22 +80,17 @@ async function setupHTB(db) {
                         tags,
                         permalink
                     } = documentInfo;
-                    const hashedTags = tags.map(tag => `#${camelcase(tag)}`).join(' ');
-                    const status = `${title} ${hashedTags} ${permalink}`;
-                    try {
-                        const tweet = await client.post('statuses/update', {
-                            status
-                        });
-                        await db.get('published').push({
-                            ...documentInfo,
-                            tweetId: tweet.id_str
-                        }).write();
+                    try{
+                    console.log(documentInfo);
+                    // TODO: Publish Here
+                        await db.get('published').push(documentInfo).write();
                         await db.get('to-publish').remove({
                             permalink
                         }).write();
-                    } catch (error) {
-                        throw new Error(`${status}\n${JSON.stringify(error)}`);
-                    }
+                      }
+                      catch (error) {
+                          throw new Error(`${status}\n${JSON.stringify(error)}`);
+                      }
                 }));
             } catch (error) {
                 hexo.log.error(error);
@@ -118,6 +114,9 @@ function processDocument(updateDB) {
         const publishedPost = document.layout === 'post' && document.published;
         const publishedPage = document.layout !== 'post' && document.htbAutoPublish !== false;
         const hexoPublished = publishedPost || publishedPage;
+        const tags = document.tags
+        const tagNames = tags ? tags.map((tag) => tag.name.toLowerCase() || tag) : [];
+        if(tagNames.includes("hackthebox") || tagNames.includes("htb"))
         await updateDB(document, hexoPublished);
         return document;
     }
@@ -135,11 +134,12 @@ async function registerFilters(cleanToPublish, updateDB) {
             const post = posts.data[index];
             await updateDocumentDB(post);
         }
-        const pages = hexo.locals.get('pages');
-        for (let index = 0; index < pages.length; index++) {
-            const page = pages.data[index];
-            await updateDocumentDB(page);
-        }
+        // NOTE: Do We want to do for pages?
+        // const pages = hexo.locals.get('pages');
+        // for (let index = 0; index < pages.length; index++) {
+        //     const page = pages.data[index];
+        //     await updateDocumentDB(page);
+        // }
     }, {
         async: true
     });
